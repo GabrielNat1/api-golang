9ae6618cf560140717d16b892d138f1aee7a45a8
GabrielNatPr0 <gabriel.contessoto.ribeiro@escola.pr.gov.br>
2025-03-11 10:48:17 -0300

    feat2

--- repository/product_repository.go
+++ repository/product_repository.go
@@ -122,7 +122,6 @@ func (pr *ProductRepository) DeleteProduct(id int) error {
 
 	_, err = query.Exec(id)
 	if err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": err})
 		//fmt.Println(err)
 		return err
 	}
